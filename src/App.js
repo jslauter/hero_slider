5fd39540165656bfedee95237c8885ba995dec0f
Jonathan Slauter <jonathanslauter@gmail.com>
2022-05-11 10:19:24 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import HeroSlider from './components/HeroSlider';
+// import Navbar from './components/NavComponent'
+
+function App() {
+  return (
+    <>
+      {/* <Navbar /> */}
+      <HeroSlider />
+    </>
+  );
+}
+
+export default App;
